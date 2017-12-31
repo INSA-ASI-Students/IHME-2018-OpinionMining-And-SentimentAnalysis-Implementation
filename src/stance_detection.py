1bca8b79af0f138df70227c5a850d09f6d6e25e3
Thibault Théologien <thibault.theologien@insa-rouen.fr>
2017-12-31 14:56:15 +0100

    UPDATE - run stance detection with parameters (select method, ...)

--- src/stance_detection.py
+++ src/stance_detection.py
@@ -1,3 +1,5 @@
+import sys
+
 from utils import dependancies
 from utils import metrics
 from utils import dataset_manager as dm
@@ -7,20 +9,54 @@ from sentiment_detection import wordnetaffect as wa
 
 
 def main():
-    # dependancies.download_nltk()
-    train_dataset = dm.format(dm.load('./dataset/train.csv', ','))
-    test_dataset = dm.format(dm.load('./dataset/test.csv', ','))
-
-    wa_prediction = wa.predict(train_dataset['Tweet'])
-    wa_success_rate = metrics.success_rate(test_dataset['Sentiment'], wa_prediction)
-
-    sw_prediction = sw.predict(train_dataset['Tweet'])
-    sw_success_rate = metrics.success_rate(test_dataset['Sentiment'], sw_prediction)
-
-    print('wordnetaffect results: %s  ' % (wa_success_rate))
-    print('sentiwordnet results: %s  ' % (sw_success_rate))
+    (filename, sentiment, opinion, stance, output) = define_parameters(sys.argv)
+    dataset = dm.format(dm.load(filename, ','))
+    sentiment_prediction = predict_sentiment(sentiment, dataset)
+    if sentiment_prediction == None:
+        print('Invalid sentiment method')
+        return 1
+    else:
+        print_results('Sentiment', sentiment, dataset, sentiment_prediction)
 
     return 0
+
+
+def predict_sentiment(sentiment, dataset):
+    if sentiment == 'wordnetaffect':
+        return wa.predict(dataset['Tweet'])
+    elif sentiment == 'sentiwordnet':
+        return sw.predict(dataset['Tweet'])
+    return None
+
+
+def print_results(column, method, dataset, prediction):
+    try:
+        truth = dataset[column]
+        success_rate = metrics.success_rate(truth, prediction)
+        print('%s results: %s  ' % (method, success_rate))
+    except:
+        pass
+
+
+def define_parameters(args):
+    filename = './dataset/train.csv'
+    sentiment = 'wordnetaffect'
+    opinion = 'default'
+    stance = 'default'
+    output = './output.csv'
+
+    for arg in args:
+        if arg.startswith('--filename='):
+            filename = arg.split('--filename=')[1]
+        elif arg.startswith('--sentiment='):
+            sentiment = arg.split('--sentiment=')[1]
+        elif arg.startswith('--opinion='):
+            opinion = arg.split('--opinion=')[1]
+        elif arg.startswith('--stance='):
+            stance = arg.split('--stance=')[1]
+        elif arg.startswith('--output='):
+            output = arg.split('--output=')[1]
+    return filename, sentiment, opinion, stance, output
 
 
 if __name__ == '__main__':
